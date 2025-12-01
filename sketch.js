8f6f5638a92044209390d42cd08be4aa63d3257b
Jeremy Southern <jezzd11@outlook.com>
2025-12-01 23:53:19 +0000

    added main functionality

--- sketch.js
+++ sketch.js
@@ -1,150 +1,163 @@
-const GRID_SIZE = 20;
-const CANVAS_SIZE = 600;
-const OFFSET = GRID_SIZE /2;
-let headX = [300];
-let headY = [300];
-let xDir = 0;
-let yDir = 0;
-let foodX;
-let foodY;
-let score = 0;
+let dropPoint;
+let dropperPos =50;
+let presents = [];
+let santaRight;
+let santaLeft;
+let currentSanta;
+let elf;
+let caughtCount =0;
+let lostCount =0;
+let caughtSound;
+let lostSound;
+let currentSound;
 
+function preload()
+{
+    santaRight = loadImage('/assets/santa1.png')
+    santaLeft = loadImage('/assets/santa2.png')
+    elf = loadImage('/assets/elf.png')
+    caughtSound = loadSound('/assets/catch.mp3')
+    lostSound = loadSound('/assets/drop.mp3')
+}
 function setup()
 {
-    createCanvas(CANVAS_SIZE,CANVAS_SIZE)
-    background(0);
-    createFood();
-    frameRate(3);
-    
+    createCanvas(400,750)
+    dropPoint = random(0,width);
 }
 function draw()
 {
-    background(0);
-    checkerBoard();
-    snakeLogic();
-    circleLogic();
-    boundaryCheck(headX,headY);   
-    console.log(headX[0],headY[0])
+    background(0,140,255)
+    drawBackground();
+    drawCatcher();
+    drawDropper();
+    dropPresents();  
 }
-function boundaryCheck(x,y)
-{
-    //checks values of the snakes head against the canvas, ends game if out of bounds 
-    if (headX[0] >= CANVAS_SIZE|| headX[0] <=0 || headY[0] >= CANVAS_SIZE || headY[0]<=0 )
-    {
-        restartGame();
-    }
 
-}
-function createFood()
+function drawCatcher()
 {
-    //generates the food 
-    foodX = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
-    foodY = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
-}
-function keyPressed()
-{ 
-    if (key === 'w' || keyCode === UP_ARROW)
-    {        
-        if (!(yDir === 1))
-        {
-            xDir = 0;
-            yDir = -1;
-        }
-        //up
-    
-    }
-    if (key === 's' || keyCode === DOWN_ARROW)
+    let catcherX;
+    let size = 50;
+    if (mouseX <= 0)
     {
-        if (!(yDir === -1))
-        {
-            xDir = 0;
-            yDir = 1;
-        }
-        //down
+        catcherX = 0
     }
-    if (key === 'a' || keyCode === LEFT_ARROW)
+    if (mouseX >= width)
     {
-        if (!(xDir === 1))
-        {
-            xDir = -1;
-            yDir = 0;
-        }
-        //left
-        
+        catcherX = width - size/2
     }
-    if (key === 'd' || keyCode === RIGHT_ARROW)
+    else
     {
-        if (!(xDir === -1))
-        {
-            xDir = 1;
-            yDir = 0;
-        }
-        //right
+        catcherX = mouseX
     }
-    
+    rectMode(CENTER)
+    image(elf,catcherX,700,100,100)
     
 }
-function snakeLogic()
-{
-    
-    for(let i = headX.length - 1; i > 0; i--)
-    {
-        headX[i] = headX[i-1];
-        headY[i] = headY[i-1];
-    }
 
-    headX[0] += GRID_SIZE * xDir;
-    headY[0] += GRID_SIZE * yDir;
-
-    for(let i =0; i<headX.length; i++)
-    {    
-        fill(0);
-        square(headX[i],headY[i],GRID_SIZE);        
-    }
-    
-}
-function circleLogic()
+function drawDropper()
 {
-    circle(foodX, foodY, GRID_SIZE);
-    if ((headX[0] + OFFSET) === foodX && (headY[0] + OFFSET) === foodY)
+    if (dropperPos > dropPoint)
     {
-        createFood();
-        headX.push(headX[headX.length-1]+ GRID_SIZE)
-        headY.push(headY[headY.length-1])
+        dropperPos -= 5
+        currentSanta = santaLeft;
+    }
+    if (dropperPos < dropPoint)
+    {
+        dropperPos += 5
+        currentSanta = santaRight;
+    }
+    if (dropperPos <= dropPoint + 10 && dropperPos >= dropPoint -10)
+    {
+        let present = 
+        {
+            x: dropPoint,
+            y:50,
+            s:35,
+            done: false,
+            c1: random(255),
+            c2: random(255),
+            c3: random(255)
+        }
+        presents.push(present)
+        dropPoint = random(width)  
+    }
+    imageMode(CENTER)
+    image(currentSanta,dropperPos,50,160,100)
+}           
+
+function dropPresents()
+{
+    let offset = 50;
+    for(let i = 0; i < presents.length;i++)
+    {
+        if (presents[i].done === false)
+        {
+            drawPresent(presents[i].x,presents[i].y,presents[i].s,presents[i].c1,presents[i].c2,presents[i].c3)
+            presents[i].y += 10;
+            if (presents[i].y >= height)
+            {
+                lostPresent();
+                presents[i].done = true
+            }
+            if (
+            presents[i].x >= mouseX - 50 &&
+            presents[i].x <= mouseX + 50 &&
+            presents[i].y >= 650 &&
+            presents[i].y <= 750)                          
+            {
+                caughtPresent();
+                presents[i].done = true;
+            }
+        }
     }
 }
 
-function checkerBoard()
+function caughtPresent()
+{
+    console.log('present caught')
+    caughtCount++;
+    //caughtSound.play();
+}
+
+function lostPresent()
+{
+    console.log('present lost')
+    lostCount--;
+    //lostSound.play()
+}
+
+function drawPresent(x,y,s,c1,c2,c3)
+{
+    
+    rectMode(CENTER)
+    fill(c1,c2,c3)
+    square(x,y,s)
+    //bows
+    circle(x-s/7,y-s/1.6,s/3)
+    circle(x+s/6,y-s/1.6,s/3)
+
+    strokeWeight((s/10)-1)
+    //line
+    line(x-(s/2), y,x+(s/2),y)
+    line(x,y-(s/2),x,y+(s/2))
+}
+
+function drawBackground()
 {
     noStroke();
-    let i =0;
-    for (let x = 0; x < CANVAS_SIZE; x += GRID_SIZE) 
-    {
-        i++;
-        for (let y = 0; y < CANVAS_SIZE; y += GRID_SIZE) 
-        {
-            i++;
-            if(i % 2 === 0)
-            {
-                fill (0,255,255)
-                square(x, y, GRID_SIZE);
-            }
-            else
-            { 
-                fill(127,255,212);
-                square(x, y, GRID_SIZE);
-            }
-            
-        }
-    }
+    fill(255)
+    circle(width/3.8,height/1.1,250)
+    circle(width/2 + 80,height/1.1,300)
+    fill(150,75,0)
+    square(270,520,40)
+    triangle(250,500,270,470,290,500)
+    fill(0)
+    rectMode(CENTER)
+    rect(270,530,10,20)
+    
 }
-function restartGame()
-{
-    gameOver = false;
-    headX = [300];
-    headY = [300];
-    xDir = 1;
-    yDir = 0;
-    score = 0;
-    createFood();
-}
+//add start 
+//add end 
+//add backing track
+//add counters 
+
